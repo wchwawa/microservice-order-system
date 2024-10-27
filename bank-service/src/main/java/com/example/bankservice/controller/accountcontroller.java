9c0c1d5ddb656613ca5c9ea83dfb18a23d6b12e7
wchwawa <wch19961116@gmail.com>
2024-10-27 14:57:57 +1100

    Fix delete product

---
+++ bank-service/src/main/java/com/example/bankservice/controller/accountcontroller.java
@@ -0,0 +1,10 @@
+package com.example.bankservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class accountcontroller {
+
+}
